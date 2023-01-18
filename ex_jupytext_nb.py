ab5cc5da880dae59a4ffd1b37577c28e073e46b0
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-01-18 14:30:08 +0100

    change color

--- ex_jupytext_nb.py
+++ ex_jupytext_nb.py
@@ -91,7 +91,7 @@ for i, (ax, title, err) in enumerate(errs):
     ax.set(title=title, aspect=1, xticks=[], yticks=[])
     ax.plot(x, y, "k")
     draw_error_band(ax, x, y, err=err,
-                    facecolor=f"C{i}", edgecolor="none", alpha=.3)
+                    facecolor=f"green", edgecolor="none", alpha=.3)
 
 plt.show()
 
